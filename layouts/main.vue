55f5e8b85c5d6175b9453d28e9097f8da8655082
OnMaine <volodymyr.vasylenko@futurragroup.com>
2024-09-09 16:35:38 +0200

    upd/

--- layouts/main.vue
+++ layouts/main.vue
@@ -3,15 +3,16 @@
     <h2>Extending Layouts from Lib - (1)</h2>
     <slot />
 
-    <img src="assets/bg.jpg" width="250" height="20">
+    <!-- <img src="assets/bg.jpg" width="250" height="20"> -->
   </div>
 
 </template>
 
 <style lang="scss" scoped>
   .layout {
-    background: url('assets/bg.jpg') center center no-repeat;
-    background-size: cover;
+    // background: url('assets/bg.jpg') center center no-repeat;
+    // background-size: cover;
+    
     height: 100%;
   }
 </style>
