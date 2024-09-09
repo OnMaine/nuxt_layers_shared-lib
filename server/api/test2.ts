610b9669b3fae468c3c22e0c4926935b90a9a73f
OnMaine <volodymyr.vasylenko@futurragroup.com>
2024-09-09 12:06:48 +0200

    init/

--- server/api/test2.ts
+++ server/api/test2.ts
@@ -1,1 +1,8 @@
-export default defineEventHandler(() => 'test api 2 with header from lib')
+
+export default defineCachedEventHandler(async event => {
+  const headers = useRequestHeaders()
+
+  return {
+    'test api 2 with header from lib/ Custom header:': headers['Custom header'],
+  };
+});
